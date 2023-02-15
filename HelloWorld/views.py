fa6d768c8bec2a5bce70a9cd0aa74eeba741b3e2
Sophia Benmhend <sdb9jg@virginia.edu>
2023-02-15 09:32:25 -0500

    added some basic homepage stuff (next step will be adding in a Google login button)

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello, World!')
+# def index(request):
+#     return HttpResponse('Hello, World!')
+
+
+def homeview(request):
+    return render(request, 'homepage/homepage.html')
