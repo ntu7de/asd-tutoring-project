c985098a02fd1a74b650946194fd8e8c600012fa
Patrick Yuson <ntu7de@virginia.edu>
2023-02-21 00:03:13 -0500

    reverted changes again

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,5 +148,4 @@ try:
 except ImportError:
     found = False
 
-django_heroku.settings(locals())
 
