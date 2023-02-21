68d8d2b164c980dfa871fb2d47a6c0a7703d2ecc
Skylar Haskiell <seh6fy@virginia.edu>
2023-02-21 11:32:53 -0500

    added students model

--- mainApp/models.py
+++ mainApp/models.py
@@ -7,5 +7,12 @@ class Tutor(models.Model):
     pronouns = models.CharField(max_length=20)
     major = models.CharField(max_length=100)
     fun_fact = models.CharField(max_length=200)
-
+class Student(models.Model):
+    studentID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    email = models.EmailField()
+    pronouns = models.CharField(max_length=20)
+    major = models.CharField(max_length=100)
+    fun_fact = models.CharField(max_length=200)
 # Create your models here.
