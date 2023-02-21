37c5d5cf360f1a69ba7ac8d46bd9fb2a0f82fd59
Patrick Yuson <ntu7de@virginia.edu>
2023-02-21 12:46:43 -0500

    trying to add postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,6 +88,10 @@ DATABASES = {
     }
 }
 
+# https://dev.to/giftedstan/heroku-how-to-deploy-a-django-app-with-postgres-in-5-minutes-5lk
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
