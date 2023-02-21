d9772532a023a27263f97392ad9419293c1c9092
Skylar Haskiell <seh6fy@virginia.edu>
2023-02-21 11:32:35 -0500

    added tutor model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Tutor(models.Model):
+    tstudentID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    email = models.EmailField()
+    pronouns = models.CharField(max_length=20)
+    major = models.CharField(max_length=100)
+    fun_fact = models.CharField(max_length=200)
 
 # Create your models here.
