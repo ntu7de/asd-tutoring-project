fbad56108f260bf748263c9c6fd3281156c7fa64
Skylar Haskiell <seh6fy@virginia.edu>
2023-02-21 22:23:11 -0500

    added classes url

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/', include('social_django.urls', namespace='social')),
     path("", views.home, name='home'),
+    path('classes/', views.classes, name = 'classes'),
 ]
