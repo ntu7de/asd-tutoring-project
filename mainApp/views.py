df91d258155a9db2d2e8ce363232958d54e94225
Skylar Haskiell <seh6fy@virginia.edu>
2023-02-21 22:33:58 -0500

    working on adding in SIS API

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,4 +1,6 @@
+import requests
 from django.shortcuts import render
+from mainApp.models import Classes
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -9,4 +11,12 @@ def home(request):
     return render(request, 'mainApp/home.html')
 
 def classes(request):
+    url = 'https://api.devhub.virginia.edu/v1/courses'
+    response = requests.get(url)
+    data = response.json()
+    # https://dev.to/yahaya_hk/how-to-populate-your-database-with-data-from-an-external-api-in-django-398i
+
+
+
+
     return render(request, 'mainApp/classes.html')
