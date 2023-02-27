1f63b6b3dddd11c6865654bb36bb44333dff1daf
Sophia Benmhend <sdb9jg@virginia.edu>
2023-02-27 12:47:16 -0500

    making it so our models show up on pgadmin

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Classes
+from .models import Classes, Tutor, Student, TutorClasses, TutorTime, SessionBooked
 # Register your models here.
 @admin.register(Classes)
 class AdminClasses(admin.ModelAdmin):
     model = Classes
+
+@admin.register(Tutor)
+class AdminClasses(admin.ModelAdmin):
+    model = Tutor
+
+@admin.register(Student)
+class AdminClasses(admin.ModelAdmin):
+    model = Student
+
+@admin.register(TutorClasses)
+class AdminClasses(admin.ModelAdmin):
+    model = TutorClasses
+
+@admin.register(TutorTime)
+class AdminClasses(admin.ModelAdmin):
+    model = TutorTime
+@admin.register(SessionBooked)
+class AdminClasses(admin.ModelAdmin):
+        model = SessionBooked
+
