aa62e22a6e8dcff590c373d816c10179b178e5ed
jackehoe <123594374+jackehoe@users.noreply.github.com>
2023-03-19 17:27:01 -0400

    made another test

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.test import SimpleTestCase
 from mainApp.models import User
 from mainApp.models import Profile
 from mainApp.models import Tutor
@@ -12,7 +11,8 @@ class TutorTestCase(TestCase):
         self.user.profile = Profile.objects.create(user=self.user, first_name="f_name", last_name="l_name", year=1,
                                                    email="email", pronouns="pronouns", major="major", is_tutor=True,
                                                    is_student=False, fun_fact="fact")
-
+        self.user.tutor = Tutor.objects.create(user=self.user, hourly_rate=10, monday_hours="10-2", tuesday_hours="5-6",
+                                               wednesday_hours="10-2", thursday_hours="5-6", friday_hours="none")
 
     def test_tutor_profile(self):
         """
@@ -28,3 +28,12 @@ class TutorTestCase(TestCase):
         self.assertEqual(tutor.is_tutor, True)
         self.assertEqual(tutor.is_student, False)
         self.assertEqual(tutor.fun_fact, 'fact')
+
+    def test_tutor_hours(self):
+        tutor = self.user.tutor
+        self.assertEqual(tutor.hourly_rate, 10)
+        self.assertEqual(tutor.monday_hours, "10-2")
+        self.assertEqual(tutor.tuesday_hours, "5-6")
+        self.assertEqual(tutor.wednesday_hours, "10-2")
+        self.assertEqual(tutor.thursday_hours, "5-6")
+        self.assertEqual(tutor.friday_hours, "none")
