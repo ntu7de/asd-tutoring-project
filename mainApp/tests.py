df23241b62d113a5d67ec384a5f890bdef82f5ca
jackehoe <123594374+jackehoe@users.noreply.github.com>
2023-04-02 23:19:55 -0400

    Made new tests

--- mainApp/tests.py
+++ mainApp/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
-from mainApp.models import User, Profile, Tutor, Classes, tutorClasses
+from mainApp.models import User, Profile, Tutor, Classes
+from django.shortcuts import get_object_or_404
 # Create your tests here.
 
 
@@ -46,7 +47,7 @@ class TutorTestCase(TestCase):
         self.assertEqual(tutor.friday_start, "9:00 AM")
         self.assertEqual(tutor.friday_end, "12:00 PM")
 
-    def test_accountDisplay(self):
+    def test_account_display(self):
         """
         test to see if the accountDisplay page works correctly
         """
@@ -57,7 +58,7 @@ class TutorTestCase(TestCase):
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
-        # Check that the rendered context contains 1 class.
+        # Check that the rendered profile info is correct
         self.assertEqual(profile.first_name, 'f_name')
         self.assertEqual(profile.last_name, 'l_name')
         self.assertEqual(profile.year, "1")
@@ -65,6 +66,34 @@ class TutorTestCase(TestCase):
         self.assertEqual(profile.major, 'major')
         self.assertEqual(profile.tutor_or_student, "tutor")
         self.assertEqual(profile.fun_fact, 'fact')
+
+
+    def test_client_status_code(self):
+        """
+        test multiple client response status codes to make sure they are all 200 OK.
+        """
+        self.client.force_login(self.user) # login
+
+        login_response = self.client.get('/login/')
+        self.assertEqual(login_response.status_code, 200)
+
+        # accountSettings isn't being used anymore
+        # account_settings_response = self.client.get('/accountSettings/')
+        # self.assertEqual(account_settings_response.status_code, 200)
+
+        tutor_response = self.client.get('/tutor/')
+        self.assertEqual(tutor_response.status_code, 200)
+
+        # tutorSettings doesn't work perfectly yet
+        # tutor_settings_response = self.client.get('/tutorsetting/')
+        # self.assertEqual(tutor_settings_response.status_code, 200)
+
+        student_settings_response = self.client.get('/studentsetting/')
+        self.assertEqual(student_settings_response.status_code, 200)
+
+        student_response = self.client.get('/student/')
+        self.assertEqual(student_response.status_code, 200)
+
 
 
 # from https://docs.djangoproject.com/en/2.1/topics/testing/tools/
@@ -89,7 +118,6 @@ class ListTest(TestCase):
     def test_class_list(self):
         """
         testing if class list returns the correct amount of classes.
-        :return:
         """
         # Issue a GET request.
         response = self.client.get('/classList/')
@@ -99,3 +127,17 @@ class ListTest(TestCase):
 
         # Check that the rendered context contains 1 class.
         self.assertEqual(len(response.context['info']), 1)
+
+    def test_added_class(self):
+        """
+        testing to see if adding a different section adds to the class list total
+        """
+        # add a class
+        Classes.objects.create(subject="testing", catalognumber="1234", classsection="002", classnumber="12346",
+                               classname="testClass", instructor="testTeacher")
+        # Issue a GET request
+        response = self.client.get('/classList/')
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+        # Check that the rendered context contains 2 classes.
+        self.assertEqual(len(response.context['info']), 2)
