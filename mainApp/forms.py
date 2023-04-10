10a2a6f45689e43204f0a6c801e7ba8f226cee28
Sophia Benmhend <sdb9jg@virginia.edu>
2023-04-10 13:41:26 -0400

    got rid of the bug in accountsettings that makes it so it updates the wrong thing if you fill it in

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -9,13 +9,21 @@ Search = ( ("option1", "course mnemonic"),
            ("option2","course number"),
            ("option1", "course name")
            ) #choices for the search choice field
-Years =(
+Years1 =(
     ("First", "First"),
     ("Second", "Second"),
     ("Third", "Third"),
     ("Fourth", "Fourth"), ("Grad", "Grad")
 ) #choices for the year choice field
-TimeSelections = (
+Years =(
+    ("Select a Year", "Select a Year"),
+    ("First", "First"),
+    ("Second", "Second"),
+    ("Third", "Third"),
+    ("Fourth", "Fourth"), ("Grad", "Grad")
+) #choices for the year choice field
+
+TimeSelections1 = (
     ("9:00 AM", "9:00 AM"),
     ("9:30 AM", "9:30 AM"),
     ("10:00 AM", "10:00 AM"),
@@ -40,7 +48,42 @@ TimeSelections = (
     ("7:30 PM", "7:30 PM"),
     ("8:00 PM", "8:00 PM"),
 ) #choices for the time choice field
+
+TimeSelections = (
+    ("Select Time", "Select Time"),
+    ("9:00 AM", "9:00 AM"),
+    ("9:30 AM", "9:30 AM"),
+    ("10:00 AM", "10:00 AM"),
+    ("10:30 AM", "10:30 AM"),
+    ("11:00 AM", "11:00 AM"),
+    ("11:30 AM", "11:30 AM"),
+    ("12:00 PM", "12:00 PM"),
+    ("12:30 PM", "12:30 PM"),
+    ("1:00 PM", "1:00 PM"),
+    ("1:30 PM", "1:30 PM"),
+    ("2:00 PM", "2:00 PM"),
+    ("2:30 PM", "2:30 PM"),
+    ("3:00 PM", "3:00 PM"),
+    ("3:30 PM", "3:30 PM"),
+    ("4:00 PM", "4:00 PM"),
+    ("4:30 PM", "4:30 PM"),
+    ("5:00 PM", "5:00 PM"),
+    ("5:30 PM", "5:30 PM"),
+    ("6:00 PM", "6:00 PM"),
+    ("6:30 PM", "6:30 PM"),
+    ("7:00 PM", "7:00 PM"),
+    ("7:30 PM", "7:30 PM"),
+    ("8:00 PM", "8:00 PM"),
+) #choices for the time choice field
+Pronouns1 = (
+    ("She/Her", "She/Her"),
+    ("He/Him", "He/Him"),
+    ("They/Them", "They/Them"),
+    ("She/They", "She/They"), ("He/They", "He/They"), ("Other", "Other")
+) #choices for the year choice field
+
 Pronouns = (
+    ("Select Pronouns", "Select Pronouns"),
     ("She/Her", "She/Her"),
     ("He/Him", "He/Him"),
     ("They/Them", "They/Them"),
@@ -54,9 +97,9 @@ class ProfileForm(forms.ModelForm): #the form you see when you first log in to i
     #all of the fields you input
     first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ex: Jim'}), required=True)
     last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ex: Ryan'}), required=True)
-    year = forms.ChoiceField(choices=Years, required=True)
+    year = forms.ChoiceField(choices=Years1, required=True)
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder':'ex: jimryan@virginia.edu'}), required=True)
-    pronouns = forms.ChoiceField(choices=Pronouns, required=True)
+    pronouns = forms.ChoiceField(choices=Pronouns1, required=True)
     major = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ex: Computer Science'}), required=True, max_length=100)
     tutor_or_student = forms.ChoiceField(choices=(("Tutor", 'Tutor'), ("Student", 'Student')), widget=forms.RadioSelect,
                                          required=True)
@@ -114,16 +157,16 @@ class TutorForm(forms.ModelForm):#the form you see that updates your tutor setti
 class FirstTutorForm(forms.ModelForm): #the form that you go to after first making your profile as a tutor
     #the fields you MUST fill out
     hourly_rate = forms.DecimalField(widget=forms.TextInput(attrs={'placeholder':'ex: 12.50'}), required=False)
-    monday_start = forms.ChoiceField(choices=TimeSelections, required=False)
-    monday_end = forms.ChoiceField(choices=TimeSelections, required=False)
-    tuesday_start = forms.ChoiceField(choices=TimeSelections, required=False)
-    tuesday_end = forms.ChoiceField(choices=TimeSelections, required=False)
-    wednesday_start = forms.ChoiceField(choices=TimeSelections, required=False)
-    wednesday_end = forms.ChoiceField(choices=TimeSelections, required=False)
-    thursday_start = forms.ChoiceField(choices=TimeSelections, required=False)
-    thursday_end = forms.ChoiceField(choices=TimeSelections, required=False)
-    friday_start = forms.ChoiceField(choices=TimeSelections, required=False)
-    friday_end = forms.ChoiceField(choices=TimeSelections, required=False)
+    monday_start = forms.ChoiceField(choices=TimeSelections1, required=False)
+    monday_end = forms.ChoiceField(choices=TimeSelections1, required=False)
+    tuesday_start = forms.ChoiceField(choices=TimeSelections1, required=False)
+    tuesday_end = forms.ChoiceField(choices=TimeSelections1, required=False)
+    wednesday_start = forms.ChoiceField(choices=TimeSelections1, required=False)
+    wednesday_end = forms.ChoiceField(choices=TimeSelections1, required=False)
+    thursday_start = forms.ChoiceField(choices=TimeSelections1, required=False)
+    thursday_end = forms.ChoiceField(choices=TimeSelections1, required=False)
+    friday_start = forms.ChoiceField(choices=TimeSelections1, required=False)
+    friday_end = forms.ChoiceField(choices=TimeSelections1, required=False)
     # monday_hours = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ex: 9:00 AM - 12:00 PM'}), required=False)
     # tuesday_hours = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ex: 9:00 AM - 12:00 PM'}), required=False)
     # wednesday_hours = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ex: 9:00 AM - 12:00 PM'}), required=False)
