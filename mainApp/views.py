6b87f301d98d9f3e7822c5acf0546dd87af515ce
Skylar Haskiell <106970856+Skylarhaskiell@users.noreply.github.com>
2023-04-18 09:10:49 -0400

    minor styling changes

--- mainApp/views.py
+++ mainApp/views.py
@@ -285,27 +285,6 @@ def accountSettings(request):
     form = ProfileForm()
     return render(request, 'mainApp/accountSettings.html', {"form": form})
 
-# @login_required
-# def accountSettings2s(request):
-#     if request.method == "POST":
-#         form = ProfileForm(request.POST)
-#         if form.is_valid():  # form isn't valid right now
-#             # if not form.email.to_python(request, value= str).__contains__("@"):
-#             #     return redirect('login')
-#             profile = form.save(commit=False)
-#             # makes it so that the google auth user is connected to this profile
-#             profile.user = request.user
-#             profile.save()
-#             if profile.tutor_or_student == "Tutor":  # sends you to initially filling in your tutor settings
-#                 return redirect('accountSettings2t')
-#             else:  # sends you to initially filling in your student settings
-#                 stud = Student.objects.create(user=request.user, classes="")  # creates an instance of a student
-#                 stud.save()  # saves that instance
-#                 return redirect('student')
-#         else:
-#             return redirect('accountSettings2s')
-#     form = ProfileForm()
-#     return render(request, 'mainApp/accountSettings2s.html', {"form": form})
 
 @login_required
 def classesdetail(request, classnumber):
